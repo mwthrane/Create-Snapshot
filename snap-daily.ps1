6b9fea5e8df94e93d0d54ee43bf362717ec744cd
Michael Thrane <michael@westthrane.dk>
2017-10-31 14:49:05 +0100

    addad drive letter

---
+++ snap-daily.ps1
@@ -0,0 +1,7 @@
+﻿
+##Creates a snapshot using snapdrive sdcli
+
+$Date = (Get-Date -format "dd-MM-yyyy") 
+$Time = (Get-Date -Format "HH-mm-ss")
+
+sdcli snap create -m $env:computername -s $Date-$Time-daily -D H -x -u
---
+++ snap-del.ps1
@@ -0,0 +1,4 @@
+﻿
+if($Date
+
+sdcli snap delete -m $env:computername -D G -s TEST1

b3e09b0f86f9aa4e80066bccaa072e75c489377a
Michael Thrane <michael@westthrane.dk>
2017-10-31 13:00:58 +0100

    initial release

---
+++ run.ps1
@@ -0,0 +1,9 @@
+﻿
+##Creates a snapshot using snapdrive CMDlets
+
+$Date = (Get-Date -format "dd-MM-yyyy") 
+$Time = (Get-Date -Format "HH-mm-ss")
+
+sdcli snap create -m $env:computername -s $Date-$Time -D G -x -u
+
+sdcli snap delete -m $env:computername -D G 

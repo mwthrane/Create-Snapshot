a8e18258dbb4cbbc5fe05eb8383bc309d4f13506
Michael Thrane <michael@westthrane.dk>
2017-10-31 15:08:53 +0100

    added link

--- run.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-##Creates a snapshot using snapdrive CMDlets
-
-$Date = (Get-Date -format "dd-MM-yyyy") 
-$Time = (Get-Date -Format "HH-mm-ss")
-
-sdcli snap create -m $env:computername -s $Date-$Time -D G -x -u
-
-sdcli snap delete -m $env:computername -D G 
--- snap-hourly.ps1
+++ snap-hourly.ps1
@@ -5,3 +5,4 @@ $Date = (Get-Date -format "dd-MM-yyyy")
 $Time = (Get-Date -Format "HH-mm-ss")
 
 sdcli snap create -m $env:computername -s $Date-$Time-hourly -D H -x -u
+## https://library.netapp.com/ecm/ecm_download_file/ECMP1189424
